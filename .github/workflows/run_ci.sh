d1f26658e2e7f211347f42f1bd2caefae2906299
Matthew R Hermes <mrhermes@uchicago.edu>
2023-05-02 10:51:16 -0500

    inspect run_ci.sh python paths

--- .github/workflows/run_ci.sh
+++ .github/workflows/run_ci.sh
@@ -14,6 +14,10 @@ python_path=$(which python)
 echo "Python path in run_ci.sh is ${python_path}"
 pyscf_path=$(python -c "import pyscf; print (list(pyscf.__path__)[0], end='')")
 echo "PySCF path in run_ci.sh is ${pyscf_path}"
+site_packages=$(echo "${pyscf_path%/*}")
+echo "site_packages is ${site_packages}"
+all_python_path=$(python -c "import sys; print (sys.path)")
+echo "Python paths are ${all_python_path}"
 pip install .
 
 # TODO: check if pyscf code is changed using dist-info file
