81a831c33aa21e0a55f1dccb2867b4524d204768
Matthew R Hermes <mrhermes@uchicago.edu>
2023-05-02 10:56:50 -0500

    python -m pip?

--- .github/workflows/run_ci.sh
+++ .github/workflows/run_ci.sh
@@ -9,7 +9,7 @@ sudo apt-get -qq install \
     curl
 
 python -m pip install --upgrade pip
-pip install pyscf
+python -m pip install pyscf
 python_path=$(which python)
 echo "Python path in run_ci.sh is ${python_path}"
 pyscf_path=$(python -c "import pyscf; print (list(pyscf.__path__)[0], end='')")
@@ -18,8 +18,8 @@ site_packages=$(echo "${pyscf_path%/*}")
 echo "site_packages is ${site_packages}"
 all_python_path=$(python -c "import sys; print (sys.path)")
 echo "Python paths are ${all_python_path}"
-pip install .
+python -m pip install .
 
 # TODO: check if pyscf code is changed using dist-info file
-pip uninstall -y pyscf-forge
-pip uninstall -y pyscf
+python -m pip uninstall -y pyscf-forge
+python -m pip uninstall -y pyscf
