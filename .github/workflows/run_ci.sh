15e065e20f5cbb390810d3336a59fd3aeec60d0f
Matthew R Hermes <mrhermes@uchicago.edu>
2023-05-02 10:34:10 -0500

    can run_ci.sh find pyscf after installing it?

--- .github/workflows/run_ci.sh
+++ .github/workflows/run_ci.sh
@@ -10,6 +10,10 @@ sudo apt-get -qq install \
 
 python -m pip install --upgrade pip
 pip install pyscf
+python_path=$(which python)
+echo "Python path in run_ci.sh is ${python_path}"
+pyscf_path=$(python -c "import pyscf; print (list(pyscf.__path__)[0], end='')")
+echo "PySCF path in run_ci.sh is ${pyscf_path}"
 pip install .
 
 # TODO: check if pyscf code is changed using dist-info file
