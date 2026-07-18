fd2f49de7ca9d8ed5ba1150123f2c6feb1f3186b
Matthew R Hermes <mrhermes@uchicago.edu>
2026-07-17 19:23:28 -0500

    _get_x is allegedly complete
    Except there is an overall factor of -1 (that does not affect Sz)
    that I can't account for

--- pyscf/lib/csf/csf_op.c
+++ pyscf/lib/csf/csf_op.c
@@ -182,7 +182,9 @@ double _get_vcc (uint64_t coupstr, unsigned int i, unsigned int j, unsigned int 
        Drake & Schlesinger, PRA 15 1990 (1977) (DOI:10.1103/PhysRevA.15.1990)
        "xdiag" is the irreducible graph
     */
-    return 0.5 - 3*_get_xdiag (coupstr, i, j, nspin);
+    // TODO: figure out why sign is + below! It makes no sense!
+    // Every time I derive this I get it off by a factor of -1!
+    return 0.5 + 3*_get_xdiag (coupstr, i, j, nspin);
 }
 
 double _get_szfac (uint64_t coupstr, unsigned int i, unsigned int nspin, int twoM)
@@ -226,6 +228,8 @@ double _get_x (uint64_t brastr, uint64_t ketstr,
     assert ((r!=p) || ((nr+np)==0));
     double xdiag = 1.0;
     unsigned int twoS0b, twoS0k, twoS0;
+    unsigned int offk = 0;
+    unsigned int offb = 0;
     int parity = 0;
     unsigned int i;
     unsigned int * twoSk = malloc ((nspin+1) * sizeof (unsigned int));
@@ -255,6 +259,7 @@ double _get_x (uint64_t brastr, uint64_t ketstr,
     assert ((q>0) || abs (nq) == 1); // undefined to have doubly-occupied dummy orbital
     bool qt_pp = ((nq > 0) && (nt > 0)) || ((nq < 0) && (nt < 0));
     bool pr_pp = ((np > 0) && (nr > 0)) || ((np < 0) && (nr < 0));
+    assert (qt_pp == pr_pp);
 
     // A bunch of index sanity checks
     if (p==r){ // p'r, pr'
@@ -314,14 +319,92 @@ double _get_x (uint64_t brastr, uint64_t ketstr,
     // We just skip all of these when we are doing Sz, which corresponds to t = q = 0
     if (q>0){
 
-    // q
-    //     -1**[S(q) + S'(q-1) - 1/2]
+    // t
+    //
+    //     = T'(t)                  if |nt| = 1
+    //       S'(t) - S'(t-1) + 1/2  if nt = 2
+    //       S(t) - S(t-1) + 1/2    if nt = -2
+    if (abs (nt) == 2){
+        if (nt > 0){
+            parity += twoSb[t] + 3*twoSb[t-1] + 1;
+        } else {
+            parity += twoSk[t] + 3*twoSk[t-1] + 1;
+        }
+    }
+    parity = parity % 4;
+
+    // T(i) and T'(i) strings
+    //     T(i) =
+    //     -1**[S'(i) + S(i) + 1]
     //     *
-    //     { S'(q)  S(q)  1      }
-    //     { 1/2    1/2   S(q-1) }
-    parity += twoSk[q] + twoSb[q-1] - 1; // graph signs
+    //     { 1    S(i)   S'(i+1) }
+    //     { 1/2  S'(i)  S(i-1)  }
+    for (i=t; i < q; i++){
+        if ((i==t) && (abs (nt) == 2)){
+            continue;
+        }
+        if ((i==(q-1)) && (abs (nq) == 2)){
+            continue;
+        }
+        if (nt < 0){ // T(i)
+            xdiag *= _get_wigner_6j_j41h (1, twoSk[i], twoSb[i+1], twoSb[i], twoSk[i-1]);
+        } else { // T'(i)
+            xdiag *= _get_wigner_6j_j41h (1, twoSk[i], twoSk[i+1], twoSb[i], twoSb[i-1]);
+        }
+        parity += 2 + twoSk[i] + twoSb[i];
+        parity = parity % 4;
+    }
+
+    // q
+    //     -1**[S(q) + S"(q-1) + 1/2]
+    //     *
+    //     { S'(q)  S(q)  1       }
+    //     { 1/2    1/2   S"(q-1) }
+    //
+    //     S"(q-1) = S(q-1)   if nq isin {-2,+1}
+    //             = S'(q-1)  if nq isin {+2,-1}
+    if (qt_pp){
+        assert (q>=3);
+        if (nq > 0){
+            offb = 2;
+        } else {
+            offk = 2;
+        }
+    }
+    switch (nq) {
+        case -2:
+            twoS0 = twoSk[q-1-offk];
+        case -1:
+            twoS0 = twoSb[q-1-offb];
+        case 1:
+            twoS0 = twoSk[q-1-offk];
+        case 2:
+            twoS0 = twoSb[q-1-offb];
+    }
+    parity += twoSk[q-offk] + twoS0 + 1; // graph signs
     parity = parity % 4; // remember everything is *2 until the very end
-    xdiag *= _get_wigner_6j_j41h (twoSb[q], twoSk[q], 2, 1, twoSk[q-1]);
+    xdiag *= _get_wigner_6j_j41h (twoSb[q-offb], twoSk[q-offb], 2, 1, twoS0);
+
+    // 'hill' and 'self' parity: flipping for pphh, hhpp
+    if ((q > (t+1)) && qt_pp){
+        if (nq > 0){
+            parity += twoSb[q-1-offb] + (3*twoSk[q-2-offk]) - 1;
+        } else {
+            parity += twoSk[q-1-offk] + (3*twoSb[q-2-offb]) - 1;
+        }
+    }
+    parity = parity % 4;
+
+    // 'hill' and 'self' parity: |nq| = 2
+    if (q>t){
+        switch (nq) {
+            case -2:
+                parity += twoSk[q-offk] + 3*twoSb[q-1-offb] - 1;
+            case 2:
+                parity += twoSb[q-offb] + 3*twoSk[q-1-offk] - 1;
+        }
+    }
+    parity = parity % 4;
 
     } // q>0
 
@@ -331,10 +414,17 @@ double _get_x (uint64_t brastr, uint64_t ketstr,
     //     *
     //     { 1    S'(i)   S(i)    }
     //     { 1/2  S(i-1)  S'(i-1) }
+    if (qt_pp){
+        if (nq > 0){
+            offb = 1;
+        } else {
+            offk = 1;
+        }
+    }
     for (i=q+1; i < r; i++){
-        parity += twoSb[i] + twoSk[i-1] - 1; // graph signs
+        parity += twoSb[i-offb] + twoSk[i-1-offk] - 1; // graph signs
         parity = parity % 4;
-        xdiag *= _get_wigner_6j_j41h (2, twoSb[i], twoSk[i], twoSk[i-1], twoSb[i-1]);
+        xdiag *= _get_wigner_6j_j41h (2, twoSb[i-offb], twoSk[i-offk], twoSk[i-1-offk], twoSb[i-1-offb]);
     }
 
     // r
@@ -346,56 +436,56 @@ double _get_x (uint64_t brastr, uint64_t ketstr,
     //
     //     S"(r) = S(r)   if nr isin {-2,+1}
     //           = S'(r)  if nr isin {+2,-1}
+    offb = 0;
+    offk = 0;
+    if (pr_pp){
+        if (nr > 0){
+            offb = 2;
+        } else {
+            offk = 2;
+        }
+    }
     switch (nr) {
         case -2:
-            twoS0 = twoSk[r];
+            twoS0 = twoSk[r+offk];
         case -1:
-            twoS0 = twoSb[r];
+            twoS0 = twoSb[r+offb];
         case 1:
-            twoS0 = twoSk[r];
+            twoS0 = twoSk[r+offk];
         case 2:
-            twoS0 = twoSb[r];
+            twoS0 = twoSb[r+offb];
     }
-    xdiag *= _get_wigner_6j_j41h (twoSb[r-1], twoSk[r-1], 2, 1, twoS0);
+    xdiag *= _get_wigner_6j_j41h (twoSb[r-1+offb], twoSk[r-1+offk], 2, 1, twoS0);
     parity += twoS0 + twoSb[r-1] - 1; // graph signs
     parity = parity % 4;
 
-    // flip the 'hill' and 'shelf' diagrams for p'r'qt, t'q'rp
+    // 'hill' and 'self' parity: flipping for pphh, hhpp
     if ((p > (r+1)) && pr_pp){
         if (nr > 0){
-            parity += twoSk[r+1] + (3*twoSb[r]) + 1;
+            parity += twoSk[r+1+offk] + (3*twoSb[r+offb]) + 1;
         } else {
-            parity += twoSb[r+1] + (3*twoSk[r]) - 1;
+            parity += twoSb[r+1+offb] + (3*twoSk[r+offk]) - 1;
         }
     }
     parity = parity % 4;
 
-    // 'hill' and 'shelf' diagrams
-
-    /* This might be unnecessary
-    mr = nr;
-    mp = np;
-    // edge case: np = 2 with adjacent diagrams. Factor of -1 and np -> nr
-    if (abs (np) == 2){
-        if ((p==(r+1)) || ((p==(r+2)) && pr_pp)){
-            parity += 2;
-            mr = np;
-            mp = nr;
-        }
-    } */
-
-    // 'hill' and 'shelf' diagrams
+    // 'hill' and 'self' parity: |nr| = 2
     if (p>r){
         switch (nr) {
             case -2:
-                parity += twoSk[r] + 3*twoSb[r-1] - 1;
+                parity += twoSk[r+offk] + 3*twoSb[r-1+offb] - 1;
             case 2:
-                parity += twoSb[r] + 3*twoSk[r-1] - 1;
+                parity += twoSb[r+offb] + 3*twoSk[r-1+offk] - 1;
         }
     }
     parity = parity % 4;
 
     // T(i) and T'(i) strings
+    //     T(i) =
+    //     -1**[S'(i) + S(i) + 1]
+    //     *
+    //     { 1    S(i)   S'(i+1) }
+    //     { 1/2  S'(i)  S(i-1)  }
     for (i=r; i < p; i++){
         if ((i==r) && (abs (nr) == 2)){
             continue;
@@ -412,7 +502,11 @@ double _get_x (uint64_t brastr, uint64_t ketstr,
         parity = parity % 4;
     }
 
-    // Final pairing sign flip
+    // p
+    //
+    //     = T(p-1)                 if |np| = 1
+    //       S'(p-1) - S'(p) + 1/2  if np = 2
+    //       S(p-1) - S(p) + 1/2    if np = -2
     if (abs (np) == 2){
         if (np > 0){
             parity += twoSb[p-1] + 3*twoSb[p] + 1;
